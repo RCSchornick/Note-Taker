fa57cd335acbb9730a916e608f2403d15cd0d0e5
RCSchornick <94027300+RCSchornick@users.noreply.github.com>
2022-02-05 20:12:30 -0600

    adding routes folder, including html and api routes files

---
+++ routes/apiroutes.js
@@ -0,0 +1,43 @@
+const router=require("express").Router()
+const fs=require("fs")
+let db=require("../db/db.json")
+
+router.get("/notes", (req,res)=>{
+    db=JSON.parse(fs.readFileSync("./db/db.json", "utf-8"))
+    res.json(db)
+
+
+
+})
+
+router.post("/notes", (req,res)=>{
+    let newNote={
+        title: req.body.title,
+        text: req.body.text,
+        id: Math.floor(Math.random()*1000)
+    }
+    db.push(newNote)
+    fs.writeFileSync("./db/db.json", JSON.stringify(db))
+res.json(db)
+
+
+})
+router.delete("/notes/:id", (req,res)=>{
+    const notesToKeep=[]
+    for(var i=0; i < db.length; i++){
+        if(db[i].id!=req.params.id){
+            notesToKeep.push(db[i])
+        }
+    }
+    db=notesToKeep
+    fs.writeFileSync("./db/db.json", JSON.stringify(db), (err)=>{
+        if (err) throw err
+    })
+res.json(db)
+
+
+})
+
+
+
+module.exports=router
